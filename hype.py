667ffb9eabcf4bef9227a7572dbb6a96d4e1e3df
amilae <ireeshika@gmail.com>
2021-11-17 08:00:22 +0530

    hype file added

---
+++ hype.py
@@ -0,0 +1,28 @@
+# select a random number from 1 to 10
+# from random import seed
+from random import randint
+# seed random number generator
+# seed(1)
+# generate random number
+n = randint(1, 10)
+
+if n == 1:
+    print("Only you can change your life. No one else can do it for you")
+elif n == 2:
+    print("Don't watch the clock; do what it does. Keep going")
+elif n == 3:
+    print("If you can dream it, you can do it")
+elif n == 4:
+    print("The best way to predict the future is to create it")
+elif n == 5:
+    print("The future depends on what you do today")
+elif n == 6:
+    print("If you're not willing to risk the usual, you have to risk the unusual")
+elif n == 7:
+    print("If you think you can, you can")
+elif n == 8:
+    print("Ever tried. Ever failed. No matter. Try again. Fail again. Fail better")
+elif n == 9:
+    print("The only way to do great work is to love what you do")
+elif n == 10:
+    print("Start where you are. Use what you have. Do what you can")
